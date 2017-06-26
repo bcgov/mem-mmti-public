9325325ded25e11856bbfe167fb1fd06c7cb36a2
jeznorth <twenty15creative@gmail.com>
2017-06-26 10:40:05 -0700

    Initial styling / markup development
    - Adding assets (images, fonts, js, scss)
    - Initial markup for the news and project pages

---
+++ src/assets/js/utils.js
@@ -0,0 +1,33 @@
+function scroll_style() {
+    var window_top = $(window).scrollTop();
+    var div_top = $('#anchor-point');
+
+    if (div_top.length) {
+        var anchorPoint = div_top.offset().top;
+
+        if (window_top > anchorPoint - 0) {
+            $("#header").addClass("dark");
+        } else {
+            $("#header").removeClass("dark");
+        }
+    }
+
+    if (window_top > 100) {
+        $(".scroll-top-btn").addClass("visible");
+    } else {
+        $(".scroll-top-btn").removeClass("visible");
+    }
+}
+
+
+$(function() {
+    $(window).scroll(scroll_style);
+    scroll_style();
+});
+
+
+$(function(){ 
+    $('.dropdown-item').click(function() {
+        $('#mainNav').collapse('hide');
+    });
+});
