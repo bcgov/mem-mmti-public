d40e39c5b6e7e182729ff799473640681303fd86
jeznorth <twenty15creative@gmail.com>
2017-07-27 11:41:39 -0700

    EPIC-1135 - Continued UI Development

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,7 +1,7 @@
-import {ChangeDetectorRef, ChangeDetectionStrategy, Component, OnInit} from "@angular/core";
-import {Project} from '../models/project';
-import {ProjectService} from '../services/project.service';
-import {PaginationInstance} from 'ngx-pagination';
+import { ChangeDetectorRef, ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Project } from '../models/project';
+import { ProjectService } from '../services/project.service';
+import { PaginationInstance } from 'ngx-pagination';
 
 @Component({
   selector: 'app-project',
@@ -10,20 +10,20 @@ import {PaginationInstance} from 'ngx-pagination';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProjectComponent implements OnInit {
-	results: Array<Project>;
+  results: Array<Project>;
   public loading: boolean;
-  public filter: string = '';
+  public filter: string;
   public config: PaginationInstance = {
-      id: 'custom',
-      itemsPerPage: 10,
-      currentPage: 1
+    id: 'custom',
+    itemsPerPage: 10,
+    currentPage: 1
   };
 
   constructor(private projectService: ProjectService, private _changeDetectionRef: ChangeDetectorRef) { }
 
   ngOnInit() {
     this.loading = true;
-  	this.projectService.getAll().subscribe(
+    this.projectService.getAll().subscribe(
       data => {
         this.results = data;
         this.loading = false;
