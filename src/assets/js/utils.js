2f6876c63657375b35479b8558116766e8d68f5b
jeznorth <twenty15creative@gmail.com>
2017-10-05 09:36:28 -0700

    MEM-539 - Fixed an issued with the header scroll behaviour
    Missed a page

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -1,11 +1,11 @@
 function scroll_style() {
     var window_top = $(window).scrollTop();
-    var div_top = $('#anchor-point');
+    var div_top = $('#pgTitle');
 
     if (div_top.length) {
         var anchorPoint = div_top.offset().top;
 
-        if (window_top > anchorPoint - 350) {
+        if (window_top > anchorPoint - 100) {
             $("#header").addClass("dark");
         } else {
             $("#header").removeClass("dark");
