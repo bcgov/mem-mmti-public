94f28c218cd8e71dc6fef6e81e40403dc24f1028
Zhou-Charles <BZHOUCHA@BCGOV>
2018-02-15 15:44:52 -0800

    em-340 fixed all links that were broken using ui-sref tag

--- src/app/authorizations/authorizations.component.ts
+++ src/app/authorizations/authorizations.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-authorizations',
@@ -7,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthorizationsComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router ) {
+    // Note: Angular 'fragment' attribute is not working so this is a work around until they decide to fix it
+    // Get URL and find the fragment of it which should be an ID. Scroll to said ID.
+    router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+            const url = router.parseUrl(router.url);
+            if (url.fragment) {
+              const element = document.querySelector('#' + url.fragment);
+              if (element) {
+                element.scrollIntoView();
+              }
+            }
+         }
+    });
+  }
 
   ngOnInit() {
   }
