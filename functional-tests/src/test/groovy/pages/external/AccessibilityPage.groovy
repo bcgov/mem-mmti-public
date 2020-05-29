009ccae7c294f664e86a4f21daffc2b76fb4b49b
dhemsworth <dylan.hemsworth@gmail.com>
2020-05-29 14:34:11 -0700

    Final functional test fixes, removed broken links

--- functional-tests/src/test/groovy/pages/external/AccessibilityPage.groovy
+++ functional-tests/src/test/groovy/pages/external/AccessibilityPage.groovy
@@ -4,12 +4,12 @@ import geb.Page
 
 class AccessibilityPage extends Page {
   static at = {
-    browser.withWindow({title.equals("Web Accessibility - Province of British Columbia")}, close:true) {
+    browser.withWindow({title.equals("Accessible Government - Province of British Columbia")}, close:true) {
       assert browser.getAvailableWindows().size().equals(2)
-      assert pageTitle.equals("Web Accessibility")
+      assert pageTitle.equals("Accessible Government")
     }
   }
-  static url = "https://www2.gov.bc.ca/gov/content/home/accessibility"
+  static url = "https://www2.gov.bc.ca/gov/content/home/accessible-government"
   static content = {
     pageTitle { $("h1").text() }
   }
