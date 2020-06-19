665c0842ee3b8c97d11b6100516f7c21d499e9ef
dhemsworth <dylan.hemsworth@gmail.com>
2020-06-19 16:35:25 -0700

    NRPT-180: Unit test update

--- functional-tests/src/test/groovy/pages/app/MapPage.groovy
+++ functional-tests/src/test/groovy/pages/app/MapPage.groovy
@@ -6,8 +6,8 @@ class MapPage extends BaseAppPage {
   static at = { searchBarText.equals("Mine name or location") }
   static url = "/map"
   static content = {
-    searchBarText { $("esri-map .esri-ui-top-left.esri-ui-corner form input").attr("placeholder") }
+    // searchBarText { $("esri-map .esri-ui-top-left.esri-ui-corner form input").attr("placeholder") }
     HomeBtn { $("#header .brand") }
-    SearchOptionsBtn { $(".esri-search__sources-button.esri-widget-button") }
+    // SearchOptionsBtn { $(".esri-search__sources-button.esri-widget-button") }
   }
 }
