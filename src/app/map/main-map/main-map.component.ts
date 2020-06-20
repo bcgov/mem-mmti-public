7fb0c3e45a113a7539727d67d0ff9c8da38c08bd
dhemsworth <dylan.hemsworth@gmail.com>
2020-06-19 18:22:21 -0700

    NRPT-180: Cruft cleanup from map component refs

--- src/app/map/main-map/main-map.component.ts
+++ src/app/map/main-map/main-map.component.ts
@@ -1,8 +1,7 @@
 import { Component, Input, HostBinding, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { CookieService } from 'ngx-cookie-service';
+import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-main-map',
@@ -10,16 +9,6 @@ import { CookieService } from 'ngx-cookie-service';
   styleUrls: ['./main-map.component.scss']
 })
 export class MainMapComponent implements OnInit, OnDestroy {
-  // public properties
-  webMapProperties: __esri.WebMapProperties;
-  mapViewProperties: __esri.MapViewProperties;
-  popupProperties: __esri.PopupTemplateProperties;
-  geocoderProperties: any;
-  map: __esri.Map;
-  mapView: __esri.MapView;
-
-  pointLayerTitle: string;
-  pointLayer: __esri.FeatureLayer;
 
   // this is needed to auto-open the map disclaimer modal on page load
   @ViewChild('modal') modal: ElementRef;
@@ -31,30 +20,13 @@ export class MainMapComponent implements OnInit, OnDestroy {
   @HostBinding('class.full-screen') fullScreen = true;
 
   // private fields
-  private cookieValue = 'UNKNOWN';
-  // private selectedId: string;
   private sub: Subscription;
-  private boundariesVisible: boolean;
 
   constructor(
-    private router: Router,
-    private modalService: NgbModal,
-    private cookieService: CookieService
+    private router: Router
   ) { }
 
   ngOnInit() {
-    this.boundariesVisible = this.showBoundaries;
-
-    // only show disclaimer once per user
-    if (this.showDisclaimer) {
-      this.checkDisclaimerCookie();
-    }
-
-    // need to wrap this call in a Promise to work around a bug in ng-bootstrap (`setTimeout` also works)
-    // (see https://github.com/ng-bootstrap/ng-bootstrap/issues/1775)
-    if (this.showDisclaimer) {
-      Promise.resolve().then(() => this.openModal());
-    }
   }
 
   ngOnDestroy(): void {
@@ -63,73 +35,8 @@ export class MainMapComponent implements OnInit, OnDestroy {
     }
   }
 
-  get boundariesButtonText(): string {
-    return this.boundariesVisible ? 'Hide Boundaries' : 'Show Boundaries';
-  }
-
-  toggle() {
-    this.boundariesVisible = !this.boundariesVisible;
-  }
-
   goToHomePage() {
-    this.closeModal();
     this.router.navigate(['/']);
   }
 
-  private checkDisclaimerCookie() {
-    const cookieExists = this.cookieService.check('disclaimerSeenOnce');
-    if (cookieExists) {
-      this.cookieValue = this.cookieService.get('disclaimerSeenOnce') || 'UNKNOWN';
-    }
-
-    // show map disclaimer only once. will not show on repeat visits to the site
-    this.showDisclaimer = this.cookieValue !== 'true';
-  }
-
-  // wait a year before asking again...
-  private setDisclaimerCookie() {
-    const now = new Date();
-    const nextYear = new Date(now.getFullYear() + 1, now.getMonth(), now.getDate());
-    this.cookieService.set('disclaimerSeenOnce', 'true', nextYear);
-  }
-
-  private openModal() {
-    this.modalRef = this.modalService.open(this.modal, { keyboard: false, backdrop: 'static' });
-
-    // The promise is resolved when the modal is closed...
-    this.modalRef
-      .result
-      .then(value => {
-        // If the user accepts the terms and conditions, don't ask again (in a year)
-        if (value === 'accept') {
-          this.setDisclaimerCookie();
-        }
-      })
-      .catch(() => { /* ignore */ });
-  }
-
-  private closeModal() {
-    this.modalRef.dismiss('disagree');
-  }
-
-  /* private onRouteChange(params: ParamMap) {
-    // fetch the project Id from URL/route params (if any)
-    this.selectedId = params.get('project');
-
-    // automatically show project popup on the map when coming from project details page
-    if (this.selectedId && this.mapView && this.pointLayer) {
-      this.navigateToProject(this.selectedId, this.mapView, this.pointLayer, this.animate);
-    }
-  }
-
-  private navigateToProject(id: string, view: __esri.MapView, featureLayer: __esri.FeatureLayer, animate = this.animate) {
-    let target: __esri.Graphic;
-    return utils
-      .findProjectById(featureLayer, id)
-      .then((response: __esri.FeatureSet) => {
-        target = response && response.features && response.features.length ? response.features[0] : null;
-      })
-      .then(() => utils.zoomToLocation(view, target, animate))
-      .then(() => utils.showMapPopup(view, target));
-  } */
 }
