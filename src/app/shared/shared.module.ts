4cb282976ae1c7afef1b7217d2a26fb18db96d1f
Kit Armstrong <kitarmstrongbc@gmail.com>
2020-09-25 10:19:43 -0700

    Fix merge conflict

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,8 @@ import { ObjectFilterPipe } from 'app/pipes/object-filter.pipe';
 import { OperatorFilterPipe } from 'app/pipes/operator-filter.pipe';
 import { ProjectTypeFilterPipe } from 'app/pipes/project-type-filter.pipe';
 import { ProjectStatusFilterPipe } from 'app/pipes/project-status-filter.pipe';
-<<<<<<< Updated upstream
 import { RemoveStringValuePipe } from 'app/pipes/remove-string-value.pipe';
-=======
 import { SafeHtmlPipe } from 'app/pipes/safe-html.pipe';
->>>>>>> Stashed changes
 
 @NgModule({
   imports: [
@@ -19,22 +16,16 @@ import { SafeHtmlPipe } from 'app/pipes/safe-html.pipe';
     OperatorFilterPipe,
     ProjectTypeFilterPipe,
     ProjectStatusFilterPipe,
-<<<<<<< Updated upstream
-    RemoveStringValuePipe
-=======
+    RemoveStringValuePipe,
     SafeHtmlPipe
->>>>>>> Stashed changes
   ],
   exports: [
     ObjectFilterPipe,
     OperatorFilterPipe,
     ProjectTypeFilterPipe,
     ProjectStatusFilterPipe,
-<<<<<<< Updated upstream
-    RemoveStringValuePipe
-=======
+    RemoveStringValuePipe,
     SafeHtmlPipe
->>>>>>> Stashed changes
   ]
 })
 export class SharedModule { }
