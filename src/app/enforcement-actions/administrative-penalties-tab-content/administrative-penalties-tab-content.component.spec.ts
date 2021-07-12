1eee151ac95339585c93d9d57c9df577f5760945
BcGovNeal <47604552+BcGovNeal@users.noreply.github.com>
2021-07-12 10:28:38 -0700

    NRPT-749 NRPT-729 Add user managed enforcement actions components (#365)
    * Add user managed enforcement actions components
    
    * Add guard for unpublished mines

---
+++ src/app/enforcement-actions/administrative-penalties-tab-content/administrative-penalties-tab-content.component.spec.ts
@@ -0,0 +1,44 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { ActivatedRoute } from '@angular/router';
+
+import { AdministrativePenaltiesTabContentComponent } from './administrative-penalties-tab-content.component';
+import { Project } from 'app/models/project';
+import { AdministrativePentalty } from 'app/models/administrative-penalty';
+
+describe('AdministrativePenaltiesTabContentComponent', () => {
+  let component: AdministrativePenaltiesTabContentComponent;
+  let fixture: ComponentFixture<AdministrativePenaltiesTabContentComponent>;
+  let ActivatedRouteStub;
+
+  beforeEach(async(() => {
+    // stub activated route
+    ActivatedRouteStub = {
+      parent: {
+        data: {
+          subscribe: (next: (value) => void) => {
+            next({ data: { projects: [Project], actions: [AdministrativePentalty] } });
+            return jasmine.createSpyObj('Subscription', ['unsubscribe']);
+          }
+        }
+      }
+    };
+
+    TestBed.configureTestingModule({
+      providers: [{ provide: ActivatedRoute, useValue: ActivatedRouteStub }],
+      declarations: [AdministrativePenaltiesTabContentComponent],
+      imports: [RouterTestingModule]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdministrativePenaltiesTabContentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/models/administrative-penalty.ts
@@ -0,0 +1,38 @@
+import { Legislation } from 'app/models/legislation';
+import { Penalty } from 'app/models/penalty';
+
+export class AdministrativePentalty {
+  _id: string;
+  issuedTo: string;
+  dateIssued: string;
+  legislation: Legislation;
+  penalties: Penalty[];
+  penaltiesString: string;
+  location: string;
+  mineGuid: string;
+  unlistedMine: string;
+  unlistedMineType: string;
+  documents: object[];
+
+  constructor(obj?: any) {
+    this._id = (obj && obj._id) || null;
+    this.dateIssued = (obj && obj.dateIssued) || null;
+    this.legislation = (obj && obj.legislation && new Legislation(obj.legislation)) || null;
+    this.penalties =
+      (obj && obj.penalties && obj.penalties.length && obj.penalties.map(penalty => new Penalty(penalty))) || null;
+    this.penaltiesString =
+      (this.penalties && this.penalties.map(penalty => penalty.buildPenaltyValueString()).join(', ')) || null;
+    this.location = (obj && obj.location) || null;
+    this.mineGuid = (obj && obj.mineGuid) || null;
+    this.unlistedMine = (obj && obj.unlistedMine) || null;
+    this.unlistedMineType = (obj && obj.unlistedMineType) || null;
+    this.documents = (obj && obj.documents) || [];
+
+    this.issuedTo = (obj && obj.issuedTo) || null;
+    if (obj && obj.issuedTo && obj.issuedTo.fullName) {
+      this.issuedTo = obj.issuedTo.fullName;
+    } else {
+      this.issuedTo = '';
+    }
+  }
+}
