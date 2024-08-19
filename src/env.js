e509726de7101dbdd5e94f58d7f9fd2dbb0c7d43
sggerard <sggerard1997@gmail.com>
2024-08-19 15:45:53 -0700

    Add button, env variable

--- src/env.js
+++ src/env.js
@@ -9,6 +9,7 @@
   window.__env.ENVIRONMENT = 'local';  // local | dev | test | prod
 
   window.__env.API_LOCATION = 'http://localhost:3000';
+  window.__env.NRCED_LOCATION = 'http://localhost:8080';
   window.__env.API_PATH = '/api';
   window.__env.API_PUBLIC_PATH = '/api/public';
   window.__env.GEOCODER_API = 'https://geocoder.api.gov.bc.ca';
