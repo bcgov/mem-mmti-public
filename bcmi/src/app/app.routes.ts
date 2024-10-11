846e1b3bf785f048e8542af17940766d7ea4c6d8
Ariel Rotter-Aboyoun <raarielgrace@gmail.com>
2024-10-11 08:58:50 -0700

    Cleanup

--- bcmi/src/app/app.routes.ts
+++ bcmi/src/app/app.routes.ts
@@ -1,23 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ContactComponent } from './static-pages/contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { MainMapComponent } from './map/main-map/main-map.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { LifecycleComponent } from './static-pages/lifecycle/lifecycle.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
-  },
-  {
-    path: 'contact',
-    component: ContactComponent,
-  },
-  {
-    path: 'lifecycle2',
-    component: LifecycleComponent,
   },
   {
     path: 'map',
